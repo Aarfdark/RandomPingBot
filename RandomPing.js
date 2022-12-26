3989b6062400dc4941c89c402fc77f66c8aa4a73
Aarfdark <65369421+Aarfdark@users.noreply.github.com>
2022-12-26 17:00:42 -0500

    Update RandomPing.js

--- RandomPing.js
+++ RandomPing.js
@@ -1,5 +1,4 @@
 // RUN CODE >>> 
-// cd C:\Users\aadar\Desktop\Aadarsh\Code\Discord Bot(s)\RandomPing
 // nodemon --inspect RandomPing.js
 
 const Discord = require('discord.js');
@@ -12,6 +11,8 @@ const client = new Discord.Client({
     Discord.GatewayIntentBits.GuildPresences
   ]
 });
+//grab login token (and other secret data) from json file
+const data = require('./login_token.json');
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -28,19 +29,41 @@ function pingRandom(msg) {
   //chooses number between 0-# of people in server, picks that person to ping
   guild.members.fetch()
     .then(mems => {
-      let ind = Math.floor(Math.random()*mems.size);
-      let user = Array.from(mems.values())[ind];
-      msg.channel.send(`<@${user.user.id}>` + " follow rule 3.");
+      let users = Array.from(mems.values());
+      
+      let usersFiltered = [];
+
+      //DOESN'T  LLLLLLLLLLLLLLL | uses Array.filter with [user has role valid pinging role to create array of valid users]
+      //manually double for-loop to find the people with the pinging role
+      users.forEach(u => {
+        u.roles.cache.forEach(role => {
+          if (role.name === "🎲" || role.name === "certified pinger")
+            usersFiltered.push(u.id);
+        });
+      });
+
+      let ind = Math.floor(Math.random()*usersFiltered.length);
+      
+      let member = usersFiltered[ind];
+      msg.channel.send(`<@${member}>` + " follow rule 3.");
     });
 }
 
 client.on('messageCreate', msg => {
   if (msg.author.bot) return;
+
   let m = msg.cleanContent;
   let user = msg.author.username;
+  let myChan = client.channels.cache.get(data.myChan);
+  let grantChan = client.channels.cache.get(data.grantChan);
 
-  //if the message has "ping" and the user has the "certified pinger" role, call pingRandom
+  //if the message has "@someone" and the user has the "certified pinger" role, call pingRandom
   if (m.includes('@someone')) {
+    //if @someone happens in something that's not my personal server or grant's #spam channel, it doesn't work
+    if (msg.channel !== myChan && msg.channel !== grantChan ) {
+      console.log(user + " tried to ping in an invalid channel")
+      return;
+    }
     if (msg.member.roles.cache.some(role => role.name === "certified pinger")) {
       pingRandom(msg);
       console.log(user + " activated ping at " + (new Date()) );
@@ -56,26 +79,28 @@ client.on('messageCreate', msg => {
   }
   
   //reacting to specific people's messages
-  if (user === "RealEnderCreeper") {
+  if (user === data.judge) {
     msg.react("👩‍⚖️");
-    /*msg.react("🏨");
-    msg.react("🅰️");
-    msg.react("🇹");
-    msg.react("🇺");
-    msg.react("🇸");
-    msg.react("🇭");
-    msg.react("🇦");
-    msg.react("🇷");*/
+    // msg.react("🏨");
+    // msg.react("🅰️");
+    // msg.react("🇹");
+    // msg.react("🇺");
+    // msg.react("🇸");
+    // msg.react("🇭");
+    // msg.react("🇦");
+    // msg.react("🇷");
   }
-  else if (user === "herb") {
+  else if (user === data.biden) {
     msg.react("🍤");
   } 
-  else if (user === "Bobmcbobber") {
+  else if (user === data.jrant) {
     msg.react("🤮");
     msg.react("👩‍🎤");
   }
+  else if (user === data.L) {
+    msg.react("🍍");
+    msg.react("🍕");
+  }
 });
 
-//grab login token from json file
-const data = require('./login_token.json');
 client.login(data.token);
